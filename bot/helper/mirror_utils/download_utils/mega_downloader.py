5ebb67d379a4bfbf7a38b8e9fd6bbe63ccf0a2dc
GithubGitbot <110614050+GithubGitbot@users.noreply.github.com>
2022-09-26 19:23:20 +0530

    Updating h-code with last Update Of Z mirror

--- bot/helper/mirror_utils/download_utils/mega_downloader.py
+++ bot/helper/mirror_utils/download_utils/mega_downloader.py
@@ -1,14 +1,12 @@
 from threading import Lock
 from pathlib import Path
-
-from bot import LOGGER, download_dict, download_dict_lock, MEGA_LIMIT, STOP_DUPLICATE, ZIP_UNZIP_LIMIT, LEECH_LIMIT, STORAGE_THRESHOLD
-from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, sendStatusMessage
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, setInterval
+from bot import *
+from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMessage, sendFile, sendMarkup
+from bot.helper.ext_utils.bot_utils import setInterval, get_readable_file_size
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.ext_utils.fs_utils import get_base_name, check_storage_threshold
 from ..status_utils.mega_download_status import MegaDownloadStatus
 from megasdkrestclient import MegaSdkRestClient, constants
-
 
 class MegaDownloader:
     POLLING_INTERVAL = 3
@@ -67,15 +65,8 @@ class MegaDownloader:
 
     def __onInterval(self):
         dlInfo = self.__mega_client.getDownloadInfo(self.gid)
-        if (
-            dlInfo['state']
-            in [
-                constants.State.TYPE_STATE_COMPLETED,
-                constants.State.TYPE_STATE_CANCELED,
-                constants.State.TYPE_STATE_FAILED,
-            ]
-            and self.__periodic is not None
-        ):
+        if dlInfo['state'] in [constants.State.TYPE_STATE_COMPLETED, constants.State.TYPE_STATE_CANCELED,
+            constants.State.TYPE_STATE_FAILED] and self.__periodic is not None:
             self.__periodic.cancel()
         if dlInfo['state'] == constants.State.TYPE_STATE_COMPLETED:
             self.__onDownloadComplete()
@@ -124,12 +115,18 @@ class MegaDownloader:
                 except:
                     mname = None
             if mname is not None:
+                if HTML:
+                    cap, f_name = GoogleDriveHelper().drive_list(mname, True)
+                    if cap:
+                        cap = f"File/Folder is already available in Drive. Here are the search results:\n\n{cap}"
+                        sendFile(self.__listener.bot, self.__listener.message, f_name, cap)
+                        return
                 smsg, button = GoogleDriveHelper().drive_list(mname, True)
                 if smsg:
-                    msg1 = "Someone already mirrored it for you !\nHere you go:"
+                    msg1 = "File/Folder is already available in Drive.\nHere are the search results:"
                     return sendMarkup(msg1, self.__listener.bot, self.__listener.message, button)
-        if any([STORAGE_THRESHOLD, ZIP_UNZIP_LIMIT, LEECH_LIMIT, MEGA_LIMIT]):
-            arch = any([self.__listener.isZip, self.__listener.extract, self.__listener.isLeech])
+        if any([STORAGE_THRESHOLD, ZIP_UNZIP_LIMIT, MEGA_LIMIT, LEECH_LIMIT]):
+            arch = any([self.__listener.isZip, self.__listener.extract])
             if STORAGE_THRESHOLD is not None:
                 acpt = check_storage_threshold(file_size, arch)
                 if not acpt:
@@ -141,7 +138,11 @@ class MegaDownloader:
                 msg3 = f'Failed, Zip/Unzip limit is {ZIP_UNZIP_LIMIT}GB.\nYour File/Folder size is {get_readable_file_size(file_size)}.'
                 limit = ZIP_UNZIP_LIMIT
             if LEECH_LIMIT is not None and self.__listener.isLeech:
-                msg3 = f'Failed, Leech limit is {LEECH_LIMIT}GB.\nYour File/Folder size is {get_readable_file_size(file_size)}.'
+                if MEGA_LIMIT < LEECH_LIMIT:
+                    msg3 = f'Failed, Mega limit is {MEGA_LIMIT}GB.\nYour File/Folder size is {get_readable_file_size(file_size)}.'
+                    limit = MEGA_LIMIT
+                    return sendMessage(msg3, self.__listener.bot, self.__listener.message)
+                msg3 = f'Leech limit is {LEECH_LIMIT}GB'
                 limit = LEECH_LIMIT
             elif MEGA_LIMIT is not None:
                 msg3 = f'Failed, Mega limit is {MEGA_LIMIT}GB.\nYour File/Folder size is {get_readable_file_size(file_size)}.'
