94dc976621a432db5ad809f9280f01aa81406667
irfan36bot <116106429+irfan36bot@users.noreply.github.com>
2022-10-23 19:12:46 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -48,10 +48,10 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/z_mirror")
+    buttons.buildbutton("Update Channel", "https://t.me/DS_Botz")
+    buttons.buildbutton("Support Group", "https://t.me/DSSupportGroup")
+    buttons.buildbutton("Mirror Group", "https://t.me/Mirror_Leech_DS_Group")
+    buttons.buildbutton("Developer", "https://t.me/Irfan50786")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
